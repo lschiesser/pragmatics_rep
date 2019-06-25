8bfd1655f83e53253f0e93563a3c90d5e6e61458
lschiesser <41335130+lschiesser@users.noreply.github.com>
2019-06-25 05:22:08 -0400

    added remaining trials

--- 05_views.js
+++ 05_views.js
@@ -105,7 +105,7 @@ const scenes = composeArray();
 // Here, we initialize a normal forced_choice view
 const pragmatics_1 = custom_pragmatics({
   name: 'pragmatics',
-  trials: 3,
+  trials: 15,
   data: [
     {
       context: scenes[0][3] + scenes[0][7] + scenes[0][1] + scenes[0][8] + scenes[0][0],
@@ -127,6 +127,90 @@ const pragmatics_1 = custom_pragmatics({
       goal: scenes[2][10],
       utterance: scenes[2][11],
       domain: scenes[2][9]
+    },
+    {
+      context: scenes[3][3] + scenes[3][7] + scenes[3][1] + scenes[3][8] + scenes[3][0],
+      question: scenes[3][5],
+      goal: scenes[3][10],
+      utterance: scenes[3][11],
+      domain: scenes[3][9]
+    },
+    {
+      context: scenes[4][3] + scenes[4][7] + scenes[4][1] + scenes[4][8] + scenes[4][0],
+      question: scenes[4][5],
+      goal: scenes[4][10],
+      utterance: scenes[4][11],
+      domain: scenes[4][9]
+    },
+    {
+      context: scenes[5][3] + scenes[5][7] + scenes[5][1] + scenes[5][8] + scenes[5][0],
+      question: scenes[5][5],
+      goal: scenes[5][10],
+      utterance: scenes[5][11],
+      domain: scenes[5][9]
+    },
+    {
+      context: scenes[6][3] + scenes[6][7] + scenes[6][1] + scenes[6][8] + scenes[6][0],
+      question: scenes[6][5],
+      goal: scenes[6][10],
+      utterance: scenes[6][11],
+      domain: scenes[6][9]
+    },
+    {
+      context: scenes[7][3] + scenes[7][7] + scenes[7][1] + scenes[7][8] + scenes[7][0],
+      question: scenes[7][5],
+      goal: scenes[7][10],
+      utterance: scenes[7][11],
+      domain: scenes[7][9]
+    },
+    {
+      context: scenes[8][3] + scenes[8][7] + scenes[8][1] + scenes[8][8] + scenes[8][0],
+      question: scenes[8][5],
+      goal: scenes[8][10],
+      utterance: scenes[8][11],
+      domain: scenes[8][9]
+    },
+    {
+      context: scenes[9][3] + scenes[9][7] + scenes[9][1] + scenes[9][8] + scenes[9][0],
+      question: scenes[9][5],
+      goal: scenes[9][10],
+      utterance: scenes[9][11],
+      domain: scenes[9][9]
+    },
+    {
+      context: scenes[10][3] + scenes[10][7] + scenes[10][1] + scenes[10][8] + scenes[10][0],
+      question: scenes[10][5],
+      goal: scenes[10][10],
+      utterance: scenes[10][11],
+      domain: scenes[10][9]
+    },
+    {
+      context: scenes[11][3] + scenes[11][7] + scenes[11][1] + scenes[11][8] + scenes[11][0],
+      question: scenes[11][5],
+      goal: scenes[11][10],
+      utterance: scenes[11][11],
+      domain: scenes[11][9]
+    },
+    {
+      context: scenes[12][3] + scenes[12][7] + scenes[12][1] + scenes[12][8] + scenes[12][0],
+      question: scenes[12][5],
+      goal: scenes[12][10],
+      utterance: scenes[12][11],
+      domain: scenes[12][9]
+    },
+    {
+      context: scenes[13][3] + scenes[13][7] + scenes[13][1] + scenes[13][8] + scenes[13][0],
+      question: scenes[13][5],
+      goal: scenes[13][10],
+      utterance: scenes[13][11],
+      domain: scenes[13][9]
+    },
+    {
+      context: scenes[14][3] + scenes[14][7] + scenes[14][1] + scenes[14][8] + scenes[14][0],
+      question: scenes[14][5],
+      goal: scenes[14][10],
+      utterance: scenes[14][11],
+      domain: scenes[14][9]
     }
   ]
 })
