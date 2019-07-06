324b2f81c1427bee8e8c2ad65dd01d7f61596dc7
polina-tsvilodub <42241414+polina-tsvilodub@users.noreply.github.com>
2019-07-06 02:05:00 +0200

    corrected data analysis script

--- data_analysis.R
+++ data_analysis.R
@@ -14,7 +14,7 @@ library(faintr)
 set.seed(123)
 
 #First, the data is read from a csv-file.
-d <- read.csv("results.csv")
+d <- read.csv("results.csv", header=T, sep=",")
 d1 <- read.csv("results (1).csv")
 d2 <- read.csv("results (2).csv")
 d3 <- read.csv("results (3).csv")
@@ -35,6 +35,7 @@ d <- select(d, c(trial_number, domain, goal, utterance, inferred_goal_state, lan
 # utterance is a predictor which is scaled orinally! 
 # factor utterance assigns the ordered levels to the different utterances 
 d <- mutate(d, goal = factor(goal), utterance = factor(utterance, levels=c("furchtbar", "schlecht", "okay", "gut", "hervorragend"), ordered=T), inferred_state = inferred_goal_state)
+d<- mutate(d, inferred_state = factor(inferred_state, levels=c("1", "2", "3", "4", "5"), ordered=T))
 View(d)
 
 # Remove outliers: exclude participants where German was not a native language, and time spent was too long (30 minutes total)
@@ -42,19 +43,22 @@ d <- filter(d, timeSpent <= 30)
 #exclude non-native speakers
 d <- filter(d, grepl("Deutsch", languages, ignore.case = T ))
 View(d)
+
 #Show means for each design cell: goal x utterance. 
 library(Rmisc)
+library(FSA)
 # calculate the means of the inferred states dependent on goal and utterance 
 # this is used because it gives us the 95% confidence interval as plotted in the original paper 
-dm<- summarySE(d, measurevar = "inferred_state", groupvars = c("goal", "utterance"))
-# plot graph 1 of the original paper Fig 3 .  
-ggplot(dm, aes(x=utterance, y=inferred_state, color=goal)) + geom_point()+geom_line(aes(group=goal), size=0.5)+geom_errorbar(aes(ymin=inferred_state-ci, ymax=inferred_state+ci))
+
+summ = Summarize(inferred_goal_state ~ utterance+goal+utterance*goal, data=d)
+ggplot(summ, aes(x=utterance, y=median, color=goal))+geom_errorbar(aes(ymin=Q1, ymax=Q3), size=0.7, position=position_dodge(.2))+geom_point(shape=15, size=4, position=position_dodge(.2))
 
 #Fit the model: bayesian model with monotonic effects
-d<- mutate(d, inferred_state = factor(inferred_state, levels=c("1", "2", "3", "4", "5"), ordered=T))
-model1_1 <-brm(mo(inferred_state)~utterance+goal+utterance*goal, d)
-summary(model1_1)
-marginal_effects(model1, "utterance:goal")
+
+model1_1 <-brm(inferred_state~utterance+goal+utterance*goal, data=d, family=cumulative("probit"), control = list(max_treedepth = 12))
+summary(model1_1, prob=.95)
+
+marginal_effects(model1_1, "utterance", categorical=T)
 
 # Check hypotheses:
 #Draw posterior samples
@@ -66,12 +70,12 @@ marginal_effects(model1, "utterance:goal")
 # posterior samples
 post_samples <- posterior_samples(model1_1)%>% as_tibble
 
-post_ehrlich_hervor <- post_samples$b_Intercept+post_samples$b_utteranceE4
-post_nett_hervor <- post_samples$b_Intercept+post_samples$b_goalnett+post_samples$`b_utteranceE4:goalnett`
-post_gemein_hervor <- post_samples$b_Intercept + post_samples$b_goalgemein + post_samples$`b_utteranceE4:goalgemein`
-post_ehrlich_schr <- post_samples$b_Intercept+post_samples$b_utterance.L
-post_nett_schr <- post_samples$b_Intercept + post_samples$b_goalnett + post_samples$`b_utterance.L:goalnett`
-post_gemein_schr <- post_samples$b_Intercept+post_samples$b_goalgemein+post_samples$`b_utterance.L:goalgemein`
+post_ehrlich_hervor <- post_samples$`b_Intercept[4]`+post_samples$b_utteranceE4
+post_nett_hervor <- post_samples$`b_Intercept[4]`+post_samples$`b_utteranceE4:goalnett`+post_samples$b_goalnett
+post_gemein_hervor <- post_samples$`b_Intercept[4]` + post_samples$b_goalgemein + post_samples$`b_utteranceE4:goalgemein`
+post_ehrlich_schr <- post_samples$`b_Intercept[1]`+post_samples$b_utterance.L
+post_nett_schr <- post_samples$`b_Intercept[1]` + post_samples$b_goalnett + post_samples$`b_utterance.L:goalnett`
+post_gemein_schr <- post_samples$`b_Intercept[1]`+post_samples$b_goalgemein+post_samples$`b_utterance.L:goalgemein`
 
 mean(mean(post_ehrlich_hervor)>mean(post_nett_hervor))
 mean(mean(post_nett_hervor)>mean(post_gemein_hervor))
@@ -81,21 +85,22 @@ mean(mean(post_ehrlich_schr)<mean(post_gemein_schr))
 #Fit a model with random by-item efects
 #Check same hypotheses 
 
-model2 <- brm(mo(inferred_state) ~ utterance+goal+utterance*goal+(1|domain), d)
+model2 <- brm(inferred_state ~ utterance+goal+utterance*goal+(1|domain), d, family=cumulative("probit"))
 summary(model2)
 
 # check same hypotheses
 post_samples2 <- posterior_samples(model2)%>% as_tibble
 
-post_ehrlich_hervor2 <- post_samples2$b_Intercept+post_samples2$b_utteranceE4
-post_nett_hervor2 <- post_samples2$b_Intercept+post_samples2$b_goalnett+post_samples2$`b_utteranceE4:goalnett`
-post_gemein_hervor2 <- post_samples2$b_Intercept + post_samples2$b_goalgemein + post_samples2$`b_utteranceE4:goalgemein`
-post_ehrlich_schr2 <- post_samples2$b_Intercept+post_samples2$b_utterance.L
-post_nett_schr2 <- post_samples2$b_Intercept + post_samples2$b_goalnett + post_samples2$`b_utterance.L:goalnett`
-post_gemein_schr2 <- post_samples2$b_Intercept+post_samples2$b_goalgemein+post_samples2$`b_utterance.L:goalgemein`
+post_ehrlich_hervor2 <- post_samples2$`b_Intercept[4]`+post_samples2$b_utteranceE4
+post_nett_hervor2 <- post_samples2$`b_Intercept[4]`+post_samples2$`b_utteranceE4:goalnett`+post_samples2$b_goalnett
+post_gemein_hervor2 <- post_samples2$`b_Intercept[4]` + post_samples2$b_goalgemein + post_samples2$`b_utteranceE4:goalgemein`
+post_ehrlich_schr2 <- post_samples2$`b_Intercept[1]`+post_samples2$b_utterance.L
+post_nett_schr2 <- post_samples2$`b_Intercept[1]` + post_samples2$b_goalnett + post_samples2$`b_utterance.L:goalnett`
+post_gemein_schr2 <- post_samples2$`b_Intercept[1]`+post_samples2$b_goalgemein+post_samples2$`b_utterance.L:goalgemein`
 
 mean(mean(post_ehrlich_hervor2)>mean(post_nett_hervor2))
 mean(mean(post_nett_hervor2)>mean(post_gemein_hervor2))
 mean(mean(post_ehrlich_schr2)==mean(post_nett_schr2))
 mean(mean(post_ehrlich_schr2)<mean(post_gemein_schr2))
 
+loo(model1_1, model2)
