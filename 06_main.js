87150bbd74b3342f4f464b09fde2865899303ae4
Lukas Schießer <41335130+lschiesser@users.noreply.github.com>
2019-07-13 13:22:21 -0400

    Update 06_main.js

--- 06_main.js
+++ 06_main.js
@@ -23,7 +23,7 @@ $("document").ready(function() {
         ],
         // Here, you can specify all information for the deployment
         deploy: {
-            experimentID: "3",
+            experimentID: "2",
             serverAppURL: "https://pragmatics-ptb.herokuapp.com/api/submit_experiment/",
             // Possible deployment methods are:
             // "debug" and "directLink"
